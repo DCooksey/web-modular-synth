1a70b90560d78b1abbb00cf7d8d01fbbb9ac9cec
Dean <dean.g.cooksey@gmail.com>
2019-06-22 22:30:36 +0200

    Add drop down selection for diff types of wave generation

--- src/generators/Oscillator.js
+++ src/generators/Oscillator.js
@@ -1,9 +1,8 @@
 const Oscillator = (waveType, freq, audioCtx) => {
-    let osc = audioCtx.createOscillator();
+    const osc = audioCtx.createOscillator();
     osc.type = waveType;
     osc.frequency.setValueAtTime(freq, audioCtx.currentTime);
     osc.connect(audioCtx.destination);
-
     return osc;
 };
 
--- src/synth/WaveGenerator.jsx
+++ src/synth/WaveGenerator.jsx
@@ -4,6 +4,10 @@ import SquareButton from "../control-units/buttons/SquareButton";
 import NumericalInput from "../control-units/inputs/NumericalInput";
 import { default as btnState } from "../control-units/buttons/SquareButtonStateEnum";
 import Oscillator from "../generators/Oscillator";
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const styles = theme => ({
     root: {
@@ -17,7 +21,8 @@ const styles = theme => ({
 const WaveGenerator = ({ classes, audioCtx }) => {
     const [generate, setGenerate] = useState(btnState.UNTOUCHED);
     const [freq, setFreq] = useState(440);
-    const [osc, setOsc] = useState(new Oscillator("saw", 440, audioCtx));
+    const [waveType, setWaveType] = useState("sine");
+    const [osc, setOsc] = useState(new Oscillator("sine", 440, audioCtx));
     
     useEffect(() => {
         if (generate === btnState.ACTIVE) {
@@ -28,21 +33,19 @@ const WaveGenerator = ({ classes, audioCtx }) => {
                 osc.start();
             }
         }
-        else if (generate === btnState.INACTIVE) {
-            osc.stop();
-        }
-    }, [generate, osc])
+    }, [generate, osc, waveType])
 
     const handleButtonClick = () => {
         if (generate === btnState.UNTOUCHED) {
             setGenerate(btnState.ACTIVE);
         }    
         else if (generate === btnState.ACTIVE) {
+            osc.stop();
             setGenerate(btnState.INACTIVE);
         }
         else if (generate === btnState.INACTIVE) {
             setGenerate(btnState.ACTIVE);
-            setOsc(new Oscillator("sine", freq, audioCtx));
+            setOsc(new Oscillator(waveType, freq, audioCtx));
         }
     };
 
@@ -51,7 +54,13 @@ const WaveGenerator = ({ classes, audioCtx }) => {
         if (isNaN(freq)) return;
         if (generate === btnState.ACTIVE) osc.stop(0);
         setFreq(freq)
-        setOsc(new Oscillator("sine", freq, audioCtx));
+        setOsc(new Oscillator(waveType, freq, audioCtx));
+    };
+
+    const handleWaveSelectChange = event => {
+        if (generate === btnState.ACTIVE) osc.stop(0);
+        setWaveType(event.target.value);
+        setOsc(new Oscillator(event.target.value, freq, audioCtx));
     };
 
     return (
@@ -61,6 +70,18 @@ const WaveGenerator = ({ classes, audioCtx }) => {
                 purpose={handleButtonClick} 
                 buttonText={generate === btnState.ACTIVE ? "ON" : "OFF"} 
             />
+            <FormControl className={classes.margin}>
+                <InputLabel htmlFor="age-customized-select">Wave Type</InputLabel>
+                <Select
+                    value={waveType}
+                    onChange={handleWaveSelectChange}
+                >
+                <MenuItem value={"sine"}>Sine</MenuItem>
+                <MenuItem value={"square"}>Square</MenuItem>
+                <MenuItem value={"sawtooth"}>Sawtooth</MenuItem>
+                <MenuItem value={"triangle"}>Triangle</MenuItem>
+                </Select>
+            </FormControl>
             <NumericalInput handleInputChange={handleInputChange} value={freq} />
         </React.Fragment>
     )
